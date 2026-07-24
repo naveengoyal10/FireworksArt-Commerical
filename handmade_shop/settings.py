4446ac9bc296a66a7af23c10faabede430c4885e
Naveen Goyal <naveen.goyal@zensar.com>
2026-07-24 19:12:23 +0530

    setting

--- handmade_shop/settings.py
+++ handmade_shop/settings.py
@@ -10,7 +10,7 @@ load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'CHANGE_ME_TO_A_SECURE_VALUE')
 
-DEBUG = os.getenv('DEBUG', 'true') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 
 
@@ -79,16 +79,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'handmade_shop.wsgi.application'
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-if DATABASE_URL:
-    DATABASES = {'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASES = {   "default": dj_database_url.parse(       DATABASE_URL,       conn_max_age=600,        ssl_require=True   )}
 
 AUTH_PASSWORD_VALIDATORS = []
 
